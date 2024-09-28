bffe4ce04da71c3ad93b9201ad69710f6be898f7
Abdelrahman Muhammed <abdelrahman.elnegery@lexmodo.com>
2024-09-28 15:04:58 +0300

    fix

--- app/Helpers/Application/ReportAccessTokenExpiry.php
+++ app/Helpers/Application/ReportAccessTokenExpiry.php
@@ -13,10 +13,6 @@ class ReportAccessTokenExpiry
             $link = env('PLUGIN_LINK');
 
             $appName = env('APP_NAME');
-
-            Http::post('https://hooks.slack.com/services/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', [
-                'text' => '<!everyone> Help ! I can\'t get an Access token in ' . $user->name . ' store, click  <' . $link . ' | Here> to help this ' .  $appName
-            ]);
         }
 
         return true;
