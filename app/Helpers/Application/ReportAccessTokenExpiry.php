35cc40479e3fdd4831c1b7845d48da0967604107
Abdulrahman Muhahmed <abdulrahman.elnegery@gmail.com>
2024-09-28 15:05:53 +0300

    fix

--- app/Helpers/Application/ReportAccessTokenExpiry.php
+++ app/Helpers/Application/ReportAccessTokenExpiry.php
@@ -9,9 +9,7 @@ class ReportAccessTokenExpiry
     public static function report($user)
     {
         if (env("REPORT_TOKENS_EXPIRY")) {
-
             $link = env('PLUGIN_LINK');
-
             $appName = env('APP_NAME');
         }
 
